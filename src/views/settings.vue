376e4394d9a9c8c39d64ec756c7a96529879c17d
Zhenhua Lu <128296338+luzhenhua1@users.noreply.github.com>
2023-08-28 13:01:40 +0800

    Update settings.vue

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -728,8 +728,8 @@
 
       <div class="footer">
         <p class="author"
-          >MADE BY
-          <a href="http://github.com/qier222" target="_blank">QIER222</a></p
+          >Retrofit BY
+          <a href="https://www.luzhenhua.com" target="_blank">Lu Zhenhua</a></p
         >
         <p class="version">v{{ version }}</p>
 
